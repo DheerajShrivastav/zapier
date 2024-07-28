cb08f9f631abb159e66f36f0ea3b31d5ea30d0ba
Dheeraj Shrivastav <dheerajshrivastav98@gmail.com>
2024-07-28 17:08:07 +0530

    processor added

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,3 +1,47 @@
-while(1){
-    
+import { PrismaClient } from '@prisma/client'
+import { Kafka } from 'kafkajs'
+
+const TOPIC_NAME = 'zap-events'
+const kafka = new Kafka({
+  clientId: 'outbox-processor',
+  brokers: ['localhost:9092'],
+})
+
+const client = new PrismaClient()
+async function main() {
+  try {
+    const producer = kafka.producer()
+    await producer.connect()
+    while (1) {
+      const pendingRows = await client.zapRunOutbox.findMany({
+        where: {},
+        take: 10,
+      })
+      console.log(pendingRows)
+      
+        producer.send({
+          topic: TOPIC_NAME,
+          messages: pendingRows.map((r) => {
+            return{
+              value: JSON.stringify({
+                zapRunId: r.zapRunId,
+                stage: 0
+              }),
+            }
+          }),
+        })
+        await client.zapRunOutbox.deleteMany({
+          where: {
+            id: {
+              in: pendingRows.map((x) => x.id),
+            },
+          },
+        })
+        await new Promise((r) => setTimeout(r, 3000))
+      
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
+main()
